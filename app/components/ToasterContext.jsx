834fc059f6cd269d5215c99149a7bd840cc472e5
Sireesha Allu <sireeshareddy921@gmail.com>
2024-10-22 16:48:03 -0500

    Add ToasterContext component for global toast notifications

--- app/components/ToasterContext.jsx
+++ app/components/ToasterContext.jsx
@@ -1,11 +1,12 @@
-"use client"
+"use client"; // Directive for Next.js indicating that this component runs on the client-side
 
-import { Toaster } from "react-hot-toast"
+import { Toaster } from "react-hot-toast"; // Importing the Toaster component from the react-hot-toast library
 
 const ToasterContext = () => {
+  // Toaster component displays toast notifications globally in the app
   return (
-    <Toaster />
-  )
-}
+    <Toaster /> // Render the Toaster component to handle and display notifications
+  );
+};
 
-export default ToasterContext
+export default ToasterContext; // Exporting the component for use in other parts of the application
